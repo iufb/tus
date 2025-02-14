3b125fa0a7e4495fe4df70380acbf685a95f92ad
iufb <bashirov3ld2@gmail.com>
2025-02-14 20:03:00 +0500

    fix:dirname

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { FileStore } from '@tus/file-store';
 import { Server } from '@tus/server';
 import express from 'express';
-import path from 'path';
+import path, { dirname } from 'path';
 import { generateUploadURL } from './s3.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const host = '0.0.0.0';
 const port = 1080;
@@ -18,6 +21,9 @@ uploadApp.all('*', server.handle.bind(server));  // Handle all requests with tus
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Make sure the file path is correct
+});
 
 app.use((req, res, next) => {
     next();
@@ -29,9 +35,6 @@ app.get('/test', (req, res) => {
     res.status(200).send("WORKING");  // Correct response method in Express
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Make sure the file path is correct
-});
 app.get('/s3', async (req, res) => {
     const url = await generateUploadURL()
     res.send({ url })
